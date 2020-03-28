25ce7854ae946a5a3d9090e25b69c6027126fa4f
Piero Narciso <pieronarciso988@gmail.com>
2020-03-28 14:02:08 -0500

    New host allowed 192.168.1.4

--- backend/settings.py
+++ backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'velcdx=w#t*5lvr4$95x7_g$6*lrzurr!(o7+)3ehi$g2t_x2y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['assistanceapp.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['assistanceapp.herokuapp.com', '127.0.0.1', '192.168.1.4']
 
 
 # Application definition
