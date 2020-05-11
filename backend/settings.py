1bce707175b29ed56ce5c660c2e9257f623f8ba7
Piero Narciso <pieronarciso988@gmail.com>
2020-05-11 07:28:09 -0500

    User Tz

--- backend/settings.py
+++ backend/settings.py
@@ -127,13 +127,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = "Peru/Lima"
+TIME_ZONE = "Lima"
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = False
+USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
